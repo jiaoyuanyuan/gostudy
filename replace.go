1c03249ca79a417ff709985c4d8f0775dc1afd54
jiaoyuanyuan <jiaoyy2009@126.com>
2018-02-10 16:39:56 +0000

    add

---
+++ replace.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main(){
+    s := "qwertyuiop"
+    r := []rune(s)
+    copy(r[4:4+3], []rune("abc"))
+    fmt.Printf("Before: %s\n", s)    
+    fmt.Printf("After: %s\n", string(r))    
+}
